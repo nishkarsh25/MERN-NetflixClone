e6007c056d02519c0ed5613d452ea436ebf4e32c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:42:32 +0530

    Explanation of usePopularMovies Custom Hook Import Statements:
    import axios from "axios";: Imports Axios, a popular HTTP client for making requests to APIs.
    import { Popular_Movie, options } from '../utils/constant';: Imports Popular_Movie and options from ../utils/constant. Popular_Movie likely represents the endpoint URL for fetching popular movies, and options contains configuration settings (such as headers or authentication details) for Axios requests.
    import { getPopularMovie } from '../redux/movieSlice';: Imports the getPopularMovie action creator from ../redux/movieSlice. This action creator is used to update the Redux store with the list of popular movies.
    import { useDispatch } from "react-redux";: Imports useDispatch from react-redux. This hook function allows components or custom hooks to dispatch actions to the Redux store.

---
+++ netflix/src/hooks/usePopularMovies.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { Popular_Movie, options } from '../utils/constant';
+import { getPopularMovie } from '../redux/movieSlice';
+import {useDispatch} from "react-redux";
+
+
+
+
