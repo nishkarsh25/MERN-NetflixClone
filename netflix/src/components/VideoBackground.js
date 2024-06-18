cf2508d4d411c50e3ed4923ccdf65c335115f1af
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 07:01:18 +0530

    Redux State Access:
    const trailerMovie = useSelector(store => store.movie.trailerMovie);:
    Uses useSelector to access trailerMovie from the Redux store, which likely holds information about the trailer of the movie.

--- netflix/src/components/VideoBackground.js
+++ netflix/src/components/VideoBackground.js
@@ -3,7 +3,7 @@ import useMovieById from '../hooks/useMovieById';
 import {useSelector} from "react-redux";
 
 const VideoBackground = ({movieId,bool}) => {
-    
+    const trailerMovie = useSelector(store=>store.movie.trailerMovie);
     
     
 
