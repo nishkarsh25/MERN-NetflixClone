c1e727bb39dc0bf4b21cc292ec9d3fdfe750bc04
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 07:01:38 +0530

    JSX Structure:
    <div className='w-[vw] overflow-hidden'>:
    
    Sets the container div's width to the viewport width and hides overflow content.
    <iframe ...>:
    
    Renders an iframe element to display the YouTube video.
    className={${bool ? "w-[100%]" : "w-screen aspect-video"}}:
    Dynamically sets the iframe's width based on the value of bool.
    w-[100%]: Sets the width to 100% of its container if bool is true.
    w-screen aspect-video: Applies Tailwind CSS classes to maintain aspect ratio and fit the screen width if bool is false.
    src={https://www.youtube.com/embed/${trailerMovie?.key}?si=HorxQfzFY2_TAO1W&autoplay=1&mute=1`}`:
    Constructs the YouTube embed URL with the key extracted from trailerMovie.
    Includes parameters autoplay=1 (auto-plays the video) and mute=1 (mutes the video).
    title="YouTube video player": Sets the title attribute for accessibility.
    frameBorder="0": Specifies no border around the iframe.
    allowFullScreen: Allows the iframe to be viewed in fullscreen mode.

--- netflix/src/components/VideoBackground.js
+++ netflix/src/components/VideoBackground.js
@@ -7,6 +7,16 @@ const VideoBackground = ({movieId,bool}) => {
     
     useMovieById(movieId);
 
-    
+    return (
+        <div className='w-[vw] overflow-hidden'>
+            <iframe
+                className={`${bool ? "w-[100%]" : "w-screen aspect-video" } `}
+                src={`https://www.youtube.com/embed/${trailerMovie?.key}?si=HorxQfzFY2_TAO1W&autoplay=1&mute=1`}
+                title="YouTube video player"
+                frameBorder="0"
+                allowFullScreen>
+            </iframe>
+        </div>
+    )
 }
 
