fb3e4419ce4be44c0d24ac43b7faaf105b2375e1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:51:36 +0530

    initialState: Defines the initial state of the slice, which includes two properties: movieName: Initially set to null, intended to store the name of the movie being searched. searchedMovie: Initially set to null, intended to store the details or results of the searched movie(s).

--- netflix/src/redux/searchSlice.js
+++ netflix/src/redux/searchSlice.js
@@ -2,5 +2,9 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const searchSlice = createSlice({
     name:"search",
+    initialState:{
+        movieName:null,
+        searchedMovie:null
+    },
     
 });
