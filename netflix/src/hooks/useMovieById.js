be2793560a39b411ed6b0d0b0fe8a3abf080af1c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:38:33 +0530

    Explanation of useMovieById Custom Hook Import Statements:
    import axios from "axios";: Imports Axios, a popular HTTP client for making requests to APIs.
    import { options } from '../utils/constant';: Imports options from ../utils/constant. These options likely include configuration settings for Axios requests, such as headers or authentication details.
    import { useDispatch } from "react-redux";: Imports useDispatch from react-redux, which allows components to dispatch actions to the Redux store.
    import { getTrailerMovie } from '../redux/movieSlice';: Imports the getTrailerMovie action creator from ../redux/movieSlice, which is used to update the Redux store with trailer information.
    import { useEffect } from "react";: Imports useEffect from React, which allows performing side effects in function components.

---
+++ netflix/src/hooks/useMovieById.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { options } from '../utils/constant';
+import { useDispatch } from "react-redux";
+import { getTrailerMovie } from '../redux/movieSlice';
+import { useEffect } from "react";
+
+
+
+
