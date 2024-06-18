78f787206d1cf03162f1e74fa8da7fe46573c6c9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:43:49 +0530

    Error Handling:
    catch (error) { console.log(error); }: Catches and logs any errors that occur during the Axios request. This ensures that any network errors or API response errors are handled gracefully and logged for debugging purposes.

--- netflix/src/hooks/usePopularMovies.js
+++ netflix/src/hooks/usePopularMovies.js
@@ -7,9 +7,10 @@ import {useDispatch} from "react-redux";
 const usePopularMovies = async () => {
     const dispatch = useDispatch();
     try {
-        
+        const res = await axios.get(Popular_Movie, options);
+        dispatch(getPopularMovie(res.data.results))
     } catch (error) {
-       
+        console.log(error);
     }
 }
 
