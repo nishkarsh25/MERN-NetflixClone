340897d81290d8c3a9117d1917140d2607574eeb
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:54:19 +0530

    Importing Reducers:
    import userReducer from "./userSlice";: This imports the reducer function from userSlice.js, which manages user-related state using createSlice from Redux Toolkit.
    import movieReducer from "./movieSlice";: This imports the reducer function from movieSlice.js, which manages movie-related state using createSlice from Redux Toolkit.
    import searchSlice from "./searchSlice";: This imports the reducer function from searchSlice.js, which manages search-related state using createSlice from Redux Toolkit.

--- netflix/src/redux/store.js
+++ netflix/src/redux/store.js
@@ -3,4 +3,10 @@ import userReducer from "./userSlice";
 import movieReducer from "./movieSlice";
 import searchSlice from "./searchSlice";
 
-
+const store = configureStore({
+    reducer:{
+        app:userReducer,
+        movie:movieReducer,
+        searchMovie:searchSlice
+    }
+});
