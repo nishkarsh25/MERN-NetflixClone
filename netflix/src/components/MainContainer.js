aa8b12f40d70d64d2979fea1067857412f19e233
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:58:48 +0530

    JSX Structure:
    Renders a <div> element.
    Contains two child components:
    <VideoTitle title={title} overview={overview}/>: Passes title and overview as props to the VideoTitle component.
    <VideoBackground movieId={id}/>: Passes id as a prop to the VideoBackground component.

--- netflix/src/components/MainContainer.js
+++ netflix/src/components/MainContainer.js
@@ -9,7 +9,11 @@ const MainContainer = () => {
  
   const {overview, id, title} = movie[4];
   
-  
-  
+  return (
+    <div>
+        <VideoTitle title={title} overview={overview}/>
+        <VideoBackground movieId={id}/>
+    </div>
+  )
 }
 
