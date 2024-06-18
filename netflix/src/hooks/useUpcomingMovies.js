c4a89c0436bc06268781c9e90fc18f5a4b08ab89
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:46:04 +0530

    Explanation of useUpcomingMovies Custom Hook Import Statements:
    import axios from "axios";: Imports Axios, a popular HTTP client for making requests to APIs.
    import { getUpcomingMovie } from "../redux/movieSlice";: Imports the getUpcomingMovie action creator from ../redux/movieSlice. This action creator is used to update the Redux store with the list of upcoming movies.
    import { Upcoming_Movie, options } from "../utils/constant";: Imports Upcoming_Movie and options from ../utils/constant. Upcoming_Movie likely represents the endpoint URL for fetching upcoming movies, and options contains configuration settings (such as headers or authentication details) for Axios requests.
    import { useDispatch } from "react-redux";: Imports useDispatch from react-redux. This hook function allows components or custom hooks to dispatch actions to the Redux store.

---
+++ netflix/src/hooks/useUpcomingMovies.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import {getUpcomingMovie } from "../redux/movieSlice";
+import {Upcoming_Movie, options } from "../utils/constant";
+import {useDispatch} from "react-redux";
+
+
