ec508890098f1014f513b1c7ba1453712f43c652
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:33:35 +0530

    .catch((error) => {: If the connection fails, the catch method is called, which takes a callback function to handle the error. console.log(error);: This logs the error message to the console, providing details about why the connection failed.

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -9,6 +9,6 @@ const databaseConnection = () => {
     mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("mongodb connected successfully");
     }).catch((error)=>{
-    
+        console.log(error);
     })
 };
