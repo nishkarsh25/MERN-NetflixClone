1a84fa77e1fc8a7e3311b45309bc05648501b9d9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:46:32 +0530

    options:
    Defines an object with HTTP request options including:
    method: Specifies the HTTP method (GET, POST, etc.).
    headers: Contains headers required for the request, such as:
    accept: Specifies the expected response format (application/json).
    Authorization: Provides a JWT (JSON Web Token) for authentication and authorization purposes.

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,6 +1,12 @@
 export const API_END_POINT = "http://localhost:8080/api/v1/user";
 
-
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGJlZjViMDUzNWE3OGYwMjllMTQ0NDE5NTQ4MjM4MCIsInN1YiI6IjY1MDRhMjNkNTllOGE5MDExZWNhYTVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LTBaAb_2NPRGPr2HeGszyFDP-onLh-fiL7fzmnOFZUg'
+    }
+  };
 
 
 
