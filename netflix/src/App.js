d9ee11725a6e75e6832d99db0b9ff7002273299c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:59:48 +0530

    Return Statement (JSX): Returns a JSX expression wrapped in a <div> element. <Body />: Renders the Body component. This component presumably contains the main content and structure of the application, such as lists of movies, search functionality, etc. <Toaster />: Renders the Toaster component from react-hot-toast. This component, when mounted, provides a toast notification system that can display temporary messages or alerts to the user. <MovieDialog />: Renders the MovieDialog component. This component might be used to display additional details about a selected movie, such as its synopsis, cast, and related information.

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,7 +3,13 @@ import { Toaster } from 'react-hot-toast';
 import MovieDialog from "./components/MovieDialog";
  
 function App() {
-  
+  return (
+    <div>
+       <Body/>
+       <Toaster/>  
+       <MovieDialog/>
+    </div>
+  );
 }
 
 
