0b2ff334b9341c2b28656f49ed9c0a81d989d330
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:53:01 +0530

    initialState: Defines the initial state of the slice, which includes two properties: user: Initially set to null, intended to store user data once logged in or fetched. isLoading: Initially set to false, used to indicate whether user-related data is currently being loaded or fetched (typically used for loading spinners or indicators).

--- netflix/src/redux/userSlice.js
+++ netflix/src/redux/userSlice.js
@@ -2,7 +2,10 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
     name:"user",
-    
+    initialState:{
+        user:null,
+        isLoading:false
+    },
     
 });
 
