4e62788ce6135949e4b2f59712f43114d7ea5619
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:58:02 +0530

    Redux State (useSelector):
    const movie = useSelector(store => store.movie?.nowPlayingMovies);:
    Retrieves nowPlayingMovies array from the Redux store under the movie slice using useSelector.
    Uses optional chaining (?.) to handle cases where store.movie or store.movie.nowPlayingMovies might be undefined or null.

--- netflix/src/components/MainContainer.js
+++ netflix/src/components/MainContainer.js
@@ -3,5 +3,12 @@ import VideoTitle from './VideoTitle'
 import VideoBackground from './VideoBackground';
 import {useSelector} from "react-redux";
 
-
+const MainContainer = () => {
+  const movie = useSelector(store=>store.movie?.nowPlayingMovies);
+  if(!movie) return; // early return in react
+ 
+  const {overview, id, title} = movie[4];
+  
+  
+}
 
