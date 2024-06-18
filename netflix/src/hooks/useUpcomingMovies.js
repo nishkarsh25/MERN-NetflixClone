d39fadfa27e4a18b1dbdd04e880434069c3629f9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:46:40 +0530

    Inside the try Block:
    const res = await axios.get(Upcoming_Movie, options);: Uses axios.get to make a GET request to the Upcoming_Movie endpoint URL with options provided for configuration.
    Upon successful response (res):
    dispatch(getUpcomingMovie(res.data.results));: Dispatches the getUpcomingMovie action with res.data.results as the payload. This action updates the Redux store state with the list of upcoming movies fetched from the API.

--- netflix/src/hooks/useUpcomingMovies.js
+++ netflix/src/hooks/useUpcomingMovies.js
@@ -6,7 +6,8 @@ import {useDispatch} from "react-redux";
 const useUpcomingMovies = async () => {
     const dispatch = useDispatch();
     try {
-        
+        const res = await axios.get(Upcoming_Movie, options);
+        dispatch(getUpcomingMovie(res.data.results));
     } catch (error) {
         
     }
