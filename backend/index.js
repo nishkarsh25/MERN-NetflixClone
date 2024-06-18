2f3af913fe8adc1e1b0df9dd2d60e3bfe245c3ef
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:42:16 +0530

    Importing Modules Importing express: import express from "express";: This imports the express library, which is used to create a web server. Importing dotenv: import dotenv from "dotenv";: This imports the dotenv library, which is used to load environment variables from a .env file into process.env. Importing Database Connection Utility: import databaseConnection from "./utils/database.js";: This imports the databaseConnection function from the database.js file in the utils directory, which handles the connection to the MongoDB database. Importing cookie-parser: import cookieParser from "cookie-parser";: This imports the cookie-parser middleware, which is used to parse cookies attached to client requests. Importing User Routes: import userRoute from "./routes/userRoute.js";: This imports the userRoute router, which contains the routes for user-related operations (register, login, logout). Importing cors: import cors from "cors";: This imports the cors middleware, which is used to enable Cross-Origin Resource Sharing (CORS) for handling requests from different origins.

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+//step-1
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
+
+
+
+
+
+
+
+
+
