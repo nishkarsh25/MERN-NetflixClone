0d3a20ed18b7bec12e9ebfd3f184dbe4b233c6d4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 07:10:42 +0530

    The SearchMovie component in your React application handles the functionality for searching movies based on user input. Here's a breakdown of its structure and functionality:
    Explanation of SearchMovie Component
    State Management:
    
    const [searchMovie, setSearchMovie] = useState("");
    Manages the state for the input field where users enter the movie they want to search for.
    Redux Integration:
    
    const dispatch = useDispatch();
    Initializes the dispatch function from React-Redux's useDispatch hook to dispatch actions to the Redux store.
    const isLoading = useSelector(store => store.app.isLoading);
    Retrieves the isLoading state from the Redux store, which indicates whether an asynchronous operation is ongoing.
    const { movieName, searchedMovie } = useSelector(store => store.searchMovie);
    Destructures movieName and searchedMovie from the Redux store's searchMovie slice, which stores details of the searched movie.

---
+++ netflix/src/components/SearchMovie.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import { SEARCH_MOVIE_URL, options } from '../utils/constant';
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchMovieDetails } from '../redux/searchSlice';
+import { setLoading } from '../redux/userSlice';
+import MovieList from './MovieList';
+
+const SearchMovie = () => {
+    const [searchMovie, setSearchMovie] = useState("");
+    const dispatch = useDispatch();
+    const isLoading = useSelector(store => store.app.isLoading);
+    const { movieName, searchedMovie } = useSelector(store => store.searchMovie);
+    
+    
+
+    
+}
+
