96624ad6c33ab85fe9aa2d4857b560bfbaa769f9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:44:41 +0530

    Explanation of useTopRatedMovies Custom Hook Import Statements:
    import axios from "axios";: Imports Axios, a popular HTTP client for making requests to APIs.
    import { getTopRatedMovie } from "../redux/movieSlice";: Imports the getTopRatedMovie action creator from ../redux/movieSlice. This action creator is used to update the Redux store with the list of top-rated movies.
    import { Top_Rated_Movie, options } from "../utils/constant";: Imports Top_Rated_Movie and options from ../utils/constant. Top_Rated_Movie likely represents the endpoint URL for fetching top-rated movies, and options contains configuration settings (such as headers or authentication details) for Axios requests.
    import { useDispatch } from "react-redux";: Imports useDispatch from react-redux. This hook function allows components or custom hooks to dispatch actions to the Redux store.

---
+++ netflix/src/hooks/useTopRatedMovies.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { getTopRatedMovie } from "../redux/movieSlice";
+import { Top_Rated_Movie, options } from "../utils/constant";
+import {useDispatch} from "react-redux";
+
