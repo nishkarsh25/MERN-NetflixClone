3d294298c9034faa0bb51e7914c087bfd41c8044
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:54:15 +0530

    Explanation of Browse Component Imports:
    React, { useEffect } from 'react': Imports React and useEffect hook for managing side effects.
    Header: Imports the Header component.
    useSelector: Imports useSelector from react-redux for accessing state from the Redux store.
    useNavigate: Imports useNavigate from react-router-dom for programmatic navigation.
    MainContainer: Imports the MainContainer component.
    MovieContainer: Imports the MovieContainer component.
    useNowPlayingMovies, usePopularMovies, useTopRatedMovies, useUpcomingMovies: Imports custom hooks for fetching movies (useNowPlayingMovies, usePopularMovies, useTopRatedMovies, useUpcomingMovies).
    SearchMovie: Imports the SearchMovie component.

---
+++ netflix/src/components/Browse.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import Header from './Header';
+import { useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import MainContainer from './MainContainer';
+import MovieContainer from './MovieContainer';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import usePopularMovies from '../hooks/usePopularMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
+import SearchMovie from './SearchMovie';
+
+
+
