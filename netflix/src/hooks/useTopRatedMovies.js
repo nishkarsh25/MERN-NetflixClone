01b6df0252da887ab94e683170bd103dc47944e9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:44:51 +0530

    Defining the useTopRatedMovies Hook:
    const useTopRatedMovies = async () => { ... }: Defines an asynchronous function useTopRatedMovies without parameters. This function is intended to be used as a custom hook to fetch top-rated movies from an API.
    Using useDispatch Hook:
    
    const dispatch = useDispatch();: Retrieves the dispatch function from React Redux. This allows the hook to dispatch actions to the Redux store.

--- netflix/src/hooks/useTopRatedMovies.js
+++ netflix/src/hooks/useTopRatedMovies.js
@@ -3,3 +3,7 @@ import { getTopRatedMovie } from "../redux/movieSlice";
 import { Top_Rated_Movie, options } from "../utils/constant";
 import {useDispatch} from "react-redux";
 
+const useTopRatedMovies = async () => {
+    const dispatch = useDispatch();
+    
+}
