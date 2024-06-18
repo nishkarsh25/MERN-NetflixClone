a471488a3f29f560bd91766f3652f8b31ca8f018
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:38:38 +0530

    Logout Function Exporting Logout Function: export const Logout = async (req, res) => {: This defines and exports an asynchronous function named Logout that handles user logout requests. Clearing Token Cookie: return res.status(200).cookie("token", "", { expiresIn: new Date(Date.now()), httpOnly: true }).json({ ... });: This clears the token cookie by setting it to an empty string and setting the expiration to the current time, then returns a 200 status with a success message.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -42,5 +42,11 @@ export const Login = async(req,res)=>{
     }
 }
 
+export const Logout = async (req,res) => {
+    return res.status(200).cookie("token", "", {expiresIn:new Date(Date.now()), httpOnly:true}).json({
+        message:"User logged out successfully.",
+        success:true,
+    });
+}
 
 
