6d9b80ae7b4d0309c3be214da2f41edc5644fd85
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 07:01:04 +0530

    Functional Component Declaration:
    const VideoBackground = ({movieId, bool}) => { ... }:
    Declares a functional component named VideoBackground that accepts movieId and bool as props.

--- netflix/src/components/VideoBackground.js
+++ netflix/src/components/VideoBackground.js
@@ -2,5 +2,11 @@ import React from 'react'
 import useMovieById from '../hooks/useMovieById';
 import {useSelector} from "react-redux";
 
+const VideoBackground = ({movieId,bool}) => {
+    
+    
+    
 
+    
+}
 
