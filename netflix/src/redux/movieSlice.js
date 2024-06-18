69d85e0135dfae4cf75d68ff1b7113fa13e5ea71
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 05:49:28 +0530

    Creating movieSlice:
    const movieSlice = createSlice({ ... });: This function call creates a slice of the Redux state for managing movie-related data. The createSlice function takes an object with configuration options.
    Slice Configuration Object:
    
    name: "movie" - Specifies the name of the slice, which will be used as part of the generated action type strings.
    initialState:
    Defines the initial state of the slice, which includes several properties:
    nowPlayingMovies: Initially set to null, will hold data for movies currently playing.
    popularMovie: Initially set to null, will hold data for popular movies.
    topRatedMovies: Initially set to null, will hold data for top-rated movies.
    upcomingMovies: Initially set to null, will hold data for upcoming movies.
    toggle: Initially set to false, used for toggling UI elements.
    trailerMovie: Initially set to null, will hold data for movie trailers.
    open: Initially set to false, used for controlling modal or dialog visibility.
    id: Initially an empty string, can be used to store an identifier related to a specific movie or action.

--- netflix/src/redux/movieSlice.js
+++ netflix/src/redux/movieSlice.js
@@ -1,3 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-
+const movieSlice = createSlice({
+    name:"movie",
+    initialState:{
+        nowPlayingMovies:null,
+        popularMovie:null,
+        topRatedMovies:null,
+        upcomingMovies:null,
+        toggle:false,
+        trailerMovie:null, 
+        open:false,
+        id:"",
+    },
+    
+});
