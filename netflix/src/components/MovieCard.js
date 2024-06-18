7084ebe7ad019a5f48a81e7a14ffa4773fbd9ded
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 07:07:25 +0530

    Explanation of MovieCard Component Props:
    posterPath: A string representing the relative path to the movie's poster image.
    movieId: A unique identifier for the movie.
    Functional Component Declaration:
    
    const MovieCard = ({ posterPath, movieId }) => { ... }:
    Declares a functional component named MovieCard that accepts props { posterPath, movieId }.

---
+++ netflix/src/components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TMDB_IMG_URL } from '../utils/constant';
+import { useDispatch } from "react-redux";
+import { getId, setOpen } from '../redux/movieSlice';
+
+const MovieCard = ({ posterPath,movieId}) => {
+  
+
+  
+
+  
+
+  
+
+  
+}
+
