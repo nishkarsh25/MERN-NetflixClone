b44ab6388c06b2d59bfec5dda0d1754dbbfd844b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 06:41:13 +0530

    Defining the useNowPlayingMovies Hook:
    const useNowPlayingMovies = async () => { ... }: Defines an asynchronous function useNowPlayingMovies without parameters. This function is intended to be used as a custom hook to fetch now playing movies from an API.

--- netflix/src/hooks/useNowPlayingMovies.js
+++ netflix/src/hooks/useNowPlayingMovies.js
@@ -3,4 +3,11 @@ import { getNowPlayingMovies } from "../redux/movieSlice";
 import { Now_Playing_Movie, options } from "../utils/constant";
 import {useDispatch} from "react-redux";
 
-
+const useNowPlayingMovies = async () => {
+    const dispatch = useDispatch();
+    try {
+        
+    } catch (error) {
+        
+    }
+}
